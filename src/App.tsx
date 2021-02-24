4926632209bc5f3f49996baa0e0ee589cfd7cb05
joe.wright <joe@joejag.com>
2021-02-24 19:43:30 +0000

    chocs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Images: https://pixabay.com/images/search/chocolate%20bar/
+// https://www.dreamstime.com/photos-images/cadbury.html
+
+interface BarSearchResult {
+  year: number
+  bars: string[]
+  exact: boolean
 }
 
-export default App;
+const toSnakeCase = (string: string) => {
+  return string
+    .replace(/\W+/g, " ")
+    .split(/ |\B(?=[A-Z])/)
+    .map((word) => word.toLowerCase())
+    .join("_")
+}
+
+function App() {
+  const [year, setYear] = React.useState<number | null>(null)
+
+  const showChoc = (year: string) => {
+    setYear(+year)
+  }
+
+  const findChoc = (year: number): BarSearchResult => {
+    if (year in BARS) {
+      return { year, bars: BARS[year], exact: true }
+    }
+
+    return { year: 1905, bars: BARS[1905], exact: false }
+  }
+
+  if (year === null) {
+    return (
+      <main>
+        <h1>What is your birth year chocolate?</h1>
+        <label htmlFor="year">Year</label>
+        <select id="year" onChange={(e) => showChoc(e.target.value)}>
+          {YEARS.map((year) => (
+            <option key={year}>{year}</option>
+          ))}
+        </select>
+      </main>
+    )
+  }
+
+  const searchResults = findChoc(year)
+
+  const fallBackImage = (ev: any) => {
+    ev.target.src = "bars/dairy_milk.jpg"
+  }
+
+  return (
+    <main>
+      {searchResults.exact && <h1>In {year} these chocolate bars were launched</h1>}
+      {!searchResults.exact && <h1>Close to that year, in {searchResults.year} these chocolate bars were launched</h1>}
+
+      <ul>
+        {searchResults.bars.map((bar) => (
+          <li key={bar}>
+            <img src={`bars/${toSnakeCase(bar)}.jpg`} alt="" onError={fallBackImage} />
+            <h2>{bar}</h2>
+          </li>
+        ))}
+      </ul>
+
+      <h3>Check another year</h3>
+      <label htmlFor="year">Year</label>
+      <select id="year" onChange={(e) => showChoc(e.target.value)}>
+        {YEARS.map((year) => (
+          <option key={year}>{year}</option>
+        ))}
+      </select>
+    </main>
+  )
+}
+
+export default App
+
+const BARS = { 1905: ["Dairy Milk"] } as Record<number, string[]>
+
+const THIS_YEAR = new Date().getUTCFullYear()
+const YEARS = Array(THIS_YEAR - (THIS_YEAR - (new Date().getUTCFullYear() - 1899)))
+  .fill("")
+  .map((v, idx) => THIS_YEAR - idx)
